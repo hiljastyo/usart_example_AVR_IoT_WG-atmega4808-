f8f89baee6308e6ef457c4f3be42d60b45fa203a
hiljastyo <72727250+hiljastyo@users.noreply.github.com>
2020-10-23 06:21:06 +0300

    Add files via upload

---
+++ usart.c
@@ -0,0 +1,58 @@
+/* 
+ * File:   usart.c
+ * Author: Ville Hiljanen
+ * email: hiljastyo@gmail.com
+ *
+ * Created on 14. lokakuuta 2020, 14:13
+ */
+
+#define F_CPU 3333333
+#define USART2_BAUD_RATE(BAUD_RATE) ((float)(F_CPU * 64 / (16 * (float)BAUD_RATE)) + 0.5)
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void USART2_init(void);
+void USART2_sendChar(char c);
+void USART2_sendString(char *str);
+
+void USART2_init(void)
+{
+    PORTF.DIR &= ~PIN1_bm;
+    PORTF.DIR |= PIN0_bm;
+    
+    USART2.BAUD = (uint16_t)USART2_BAUD_RATE(9600);
+
+    USART2.CTRLB |= USART_TXEN_bm; 
+}
+
+void USART2_sendChar(char c)
+{
+    while (!(USART2.STATUS & USART_DREIF_bm))
+    {
+        ;
+    }        
+    USART2.TXDATAL = c;
+}
+
+void USART2_sendString(char *str)
+{
+    for(size_t i = 0; i < strlen(str); i++)
+    {
+        USART2_sendChar(str[i]);
+    }
+}
+
+int main(void)
+{
+    USART2_init();
+    
+    while (1) 
+    {
+        USART2_sendString("This is your string via USART\r\n");
+        _delay_ms(500);
+    }
+}
